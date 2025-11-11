520631090b5c075aaeb75cc6701fc12801b68441
Martin Berktold <martin.berktold@student.uibk.ac.at>
2025-11-11 14:39:36 +0100

    add menu to start

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -1,12 +1,15 @@
 #include "game.h"
 
 #include "../entities/player/player.h"
+#include "../ui/font_renderer.h"
+#include "../core/logger.h"
 #include "../entities/utils/controller.h"
 #include "./postprocessing.h"
 #include "./tilemanager.h"
 #include "./windowmanager.h"
 
 #include "SFML/Graphics/RenderWindow.hpp"
+#include "menu.h"
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/Sprite.hpp>
@@ -26,7 +29,17 @@ void Game::run() {
     TileManager tileManager;
     PostProcessing postProc(900, 900);
 
+    FontRenderer fontRenderer("assets/font/minecraft.ttf");
+    if (!fontRenderer.isLoaded()) {
+        Logger::error("Failed to load font for UI rendering");
+    }
+    
     sf::Clock clock;
+    
+    Menu menu(windowManager);
+    menu.show();
+
+    clock.reset();
 
     while (window.isOpen()) {
 
@@ -39,7 +52,7 @@ void Game::run() {
         if (resetClock) {
             clock.restart();
         }
-        
+
         windowManager.clear();
         controller.getPlayerView().setViewport(
             windowManager.getMainView().getViewport()
@@ -57,25 +70,20 @@ void Game::run() {
             // ui
             target.setView(windowManager.getDefaultView());
 
-            sf::Font font;
-            if (!font.openFromFile("assets/font/minecraft.ttf")) {
-                throw std::runtime_error("Failed to load font");
-            }
+            // target.setView(windowManager.getDefaultView());
+            fontRenderer.drawTextUI(target, "Inventory UI []", { std::floor(target.getView().getCenter().x - 100.f), std::floor(target.getView().getCenter().y + target.getSize().y / 2.f - 50.f) }, 24);
+            // sf::Text debugText(fontRenderer.getFont());
+            // debugText.setString("Inventory UI");
+            // debugText.setCharacterSize(24);
+            // debugText.setFillColor(sf::Color::White);
+            // debugText.setStyle(sf::Text::Bold);
+            // debugText.setPosition(
+            //     { std::floor(target.getView().getCenter().x -
+            //           debugText.getLocalBounds().size.x / 2.f),
+            //       std::floor(target.getView().getCenter().y + 
+            //           target.getSize().y / 2.f - 50.f )});
 
-            target.setView(windowManager.getDefaultView());
-
-            sf::Text debugText(font);
-            debugText.setString("Inventory UI");
-            debugText.setCharacterSize(24);
-            debugText.setFillColor(sf::Color::White);
-            debugText.setStyle(sf::Text::Bold);
-            debugText.setPosition(
-                { std::floor(target.getView().getCenter().x -
-                      debugText.getLocalBounds().size.x / 2.f),
-                  std::floor(target.getView().getCenter().y + 
-                      target.getSize().y / 2.f - 50.f )});
-
-            target.draw(debugText);
+            // target.draw(debugText);
 
         }, nullptr);
 
