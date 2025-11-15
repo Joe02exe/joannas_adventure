99e987b6111cfa83003f4ada3f80b0629b34cdcf
Joe02exe <lonza.johannes@gmail.com>
2025-11-15 21:44:01 +0100

    add interactable

---
+++ include/joanna/entities/interactable.h
@@ -0,0 +1,55 @@
+#include "joanna/entities/interactionbutton.h"
+#include "joanna/utils/logger.h"
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+class Interactable {
+
+    public:
+        Interactable(const sf::FloatRect& box, const std::optional<std::string>& name,
+          const std::string& buttonTexturePath
+      )
+          : id(NEXT_ID++), boundingBox(box), name(name),
+            button(boundingBox, buttonTexturePath) {}
+
+        virtual ~Interactable() = default;
+
+        virtual void onInteract() = 0;
+        virtual void render(sf::RenderWindow& window) = 0;
+
+        bool canPlayerInteract(const sf::Vector2f& playerPos) const {
+            // assuming that playerpos is the center of the player
+            sf::Vector2f center(
+                boundingBox.position.x + boundingBox.size.x * 0.5f,
+                boundingBox.position.y + boundingBox.size.y * 0.5f
+            );
+
+            float dx = playerPos.x - center.x;
+            float dy = playerPos.y - center.y;
+            return dx * dx + dy * dy <= interactionDistance * interactionDistance;
+        }
+
+        uint32_t getId() const {
+            return id;
+        }
+
+        std::optional<std::string> getName() const {
+            return name;
+        }
+
+        sf::FloatRect getBoundingBox() const {
+            return boundingBox;
+        }
+
+        void setName(const std::optional<std::string>& newName) {
+            name = newName;
+        }
+
+    private:
+        static inline uint32_t NEXT_ID = 1; // uuid --> uuids would require extra dependencies
+        const uint32_t id;
+        const sf::FloatRect boundingBox;
+        std::optional<std::string> name;
+        InteractionButton button;
+        const float interactionDistance = 32.f;
+};
