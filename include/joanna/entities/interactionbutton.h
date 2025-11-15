ef28dc079193e45ec80a0e1772d4dafb8eb531a9
Joe02exe <lonza.johannes@gmail.com>
2025-11-15 21:43:48 +0100

    add interactionButton

---
+++ include/joanna/entities/interactionbutton.h
@@ -0,0 +1,33 @@
+#include "joanna/utils/logger.h"
+#include <SFML/Graphics.hpp>
+
+class InteractionButton {
+    public:
+        InteractionButton(const sf::FloatRect& box, const std::string& texturePath)
+            : box(box), texture(texturePath), sprite(texture) {
+            sprite.setPosition({ box.position.x,
+                                box.position.y - box.size.y * 0.5f });
+        }
+
+        void render(sf::RenderWindow& window) {
+            window.draw(sprite);
+        }
+
+        void setTexture(const std::string& texturePath) {
+            if (!texture.loadFromFile(texturePath)) {
+                Logger::error(
+                    "Failed to load interaction button texture from {}", texturePath
+                );
+            }
+            sprite.setTexture(texture);
+        }
+
+        sf::Vector2f getPosition() const {
+            return sprite.getPosition();
+        }
+
+    private:
+        const sf::FloatRect box;
+        sf::Sprite sprite;
+        sf::Texture texture; // keep it alive
+};
